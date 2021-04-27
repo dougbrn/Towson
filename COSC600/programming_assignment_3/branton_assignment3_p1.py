65635dc2ccee68a54a2c3b85611c71594b904dbd
Doug Branton <dougrbranton@gmail.com>
2021-04-26 21:08:55 -0400

    Update branton_assignment3_p1.py

--- COSC600/programming_assignment_3/branton_assignment3_p1.py
+++ COSC600/programming_assignment_3/branton_assignment3_p1.py
@@ -2,10 +2,12 @@ import numpy as np
 
 class Node_BST():
     #Initialize a node, has a value, left child and right child
-    def __init__(self, value=None):
+    def __init__(self, value=None,depth=None):
         self.value = value
+        self.depth = depth
         self.left = None
         self.right = None
+        
 
     def search(self, search_value):
         #Return when found or if the node is empty
@@ -17,29 +19,59 @@ class Node_BST():
         else:
             return self.right.search(search_value)
 
-    def insert(self, value):
+    def insert(self, value, depth=0):
         #if root is empty, insert value at root
         if self.value == None:
             self.value = value
+            self.depth = 0
             return
         #Do not insert values already in BST
         elif self.value == value:
             return
         elif value < self.value:
+            depth+=1
             if self.left is None:
-                self.left = Node_BST(value)
+                self.left = Node_BST(value,depth)
             else:
-                self.left.insert(value)
+                self.left.insert(value,depth)
             return
         elif value > self.value:
+            depth+=1
             if self.right is None:
-                self.right = Node_BST(value)
+                self.right = Node_BST(value,depth)
             else:
-                self.right.insert(value)
+                self.right.insert(value,depth)
             return
 
     def delete(self,val):
-        pass
+        #search for the node to delete
+        if val < self.value:
+            if self.left:
+                self.left = self.left.delete(val)
+            return self
+        if val > self.value:
+            if self.right:
+                self.right = self.right.delete(val)
+            return self
+        
+        #End if node doesn't exist
+        if self == None:
+            return self
+
+        #Replacement, first look at children
+        if self.right == None:
+            return self.left
+        if self.left == None:
+            return self.right
+        #When node has two children, find maximum of left child
+        max_left_child = self.left
+        while max_left_child.right:
+            max_left_child = max_left_child.right
+        self.value = max_left_child.value
+        self.left = self.left.delete(max_left_child.value)
+        return self
+        
+        
         
     def traverse(self, order, val_list=[]):
         # Order is one of inorder, preorder, or postorder
@@ -68,15 +100,25 @@ class Node_BST():
             print("Not a valid traversal order: inorder, preorder, postorder")
         return val_list
 
+    def get_depths(self,depths=[]):
+        if self.value is not None:
+            depths.append(self.depth)
+        if self.left is not None:
+            self.left.get_depths(depths)
+        if self.right is not None:
+            self.right.get_depths(depths)
+        return depths
+
     def calc_avg_depth(self):
-        pass
+        depths = self.get_depths([])
+        return np.mean(depths)
                 
 
 if __name__ == "__main__":
-    
-    rand_ints = np.random.randint(0,50000,10)
-    print(rand_ints)
+    n=5000
+    rand_ints = np.random.randint(0,50000,n)
     BST = Node_BST()
     for rand_int in rand_ints:
         BST.insert(rand_int)
-    print(BST.traverse('inorder'))
+    avg_depth = BST.calc_avg_depth()
+    print(f"Average Depth of Binary Search Tree (n={n}): {avg_depth}")
